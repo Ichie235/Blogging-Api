6365bb0051dfccc35aeeadd33ebd16eb109e4d84
ichie chinemrem richard <chinemeremichie@gmail.com>
2023-01-11 17:57:20 +0100

    commit

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const {MONGODB_URI} = require('./config/index')
 
 // connect to mongodb
 async function connectToMongoDB() {
-    await moogoose.connect(MONGODB_URI,{ useNewUrlParser: true });
+    await moogoose.connect(MONGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true  });
 
     moogoose.connection.on('connected', () => {
         console.log('Connected to MongoDB successfully');
